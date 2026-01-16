6155d54d0b132647cca1632a9cf8f859b3892b6e
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-16 22:31:49 +0530

    Update MeanHeight.java

--- src/CoreProgramming/Arrays/Level1/MeanHeight.java
+++ src/CoreProgramming/Arrays/Level1/MeanHeight.java
@@ -1,5 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * Problem Statement:
+ * Write a Java program to calculate the mean height of a football team.
+ *
+ * The program should:
+ * - Take the height of 11 players as input
+ * - Store the heights in an array
+ * - Calculate the sum of all heights
+ * - Compute the mean height using the formula:
+ *   mean = sum of heights / number of players
+ * - Display the mean height of the football team
+ *
+ * This program demonstrates:
+ * - Use of arrays
+ * - Looping through array elements
+ * - User input handling using Scanner
+ * - Basic arithmetic operations
+ */
+
 // Class to calculate mean height
 class MeanHeight {
 
