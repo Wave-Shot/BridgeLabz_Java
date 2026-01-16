b1fed984d64cc0b99f3c725b9b4f4c02770121b9
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-16 22:41:46 +0530

    Add comments for problem statement and class description
    Added problem statement and class description comments.

--- src/CoreProgramming/Arrays/Level1/VotingEligibilityArray.java
+++ src/CoreProgramming/Arrays/Level1/VotingEligibilityArray.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+/*
+ * Problem Statement:
+ * Write a Java program to check the voting eligibility of 10 students
+ * based on their age.
+ */
+
+/*
+ * Class Name: VotingEligibilityArray
+ *
+ * Description:
+ * This class accepts the ages of 10 students, stores them in an array,
+ * checks whether each student is eligible to vote based on age criteria,
+ * and displays the appropriate message for each student.
+ */
+
 // Class to check voting eligibility for 10 students
 class VotingEligibilityArray {
 
