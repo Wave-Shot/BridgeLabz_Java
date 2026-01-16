456dc596788bbc02e8ab9fd5d038767be07585ad
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-16 22:55:38 +0530

    Update HotelBooking.java

--- src/ObjectOrientedProgramming/Constructors/Level1/HotelBooking.java
+++ src/ObjectOrientedProgramming/Constructors/Level1/HotelBooking.java
@@ -1,3 +1,19 @@
+/*
+ * Problem Statement:
+ * Write a Java program to demonstrate constructor overloading
+ * and constructor chaining using a HotelBooking class.
+ */
+
+/*
+ * Class Name: HotelBooking
+ *
+ * Description:
+ * This class represents a hotel booking system and demonstrates
+ * constructor overloading. It includes a default constructor that
+ * uses constructor chaining, a parameterized constructor, and a
+ * copy constructor to create a new object from an existing one.
+ */
+
 class HotelBooking {
 
     private String guestName;
