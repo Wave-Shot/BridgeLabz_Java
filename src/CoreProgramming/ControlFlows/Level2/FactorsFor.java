4d03dd72b556dede5abdac0208082b016fc2455d
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-17 14:48:17 +0530

    Update FactorsFor.java

--- src/CoreProgramming/ControlFlows/Level2/FactorsFor.java
+++ src/CoreProgramming/ControlFlows/Level2/FactorsFor.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+/*
+ * Problem Statement:
+ * Write a Java program to find and display all the factors
+ * of a given positive number using a for loop.
+ */
+
+/*
+ * Class Name: FactorsFor
+ *
+ * Description:
+ * This class reads a positive integer from the user and
+ * uses a for loop to find all numbers that divide the
+ * given number completely and prints them as factors.
+ */
+
 // Class to find factors using for loop
 class FactorsFor {
 
