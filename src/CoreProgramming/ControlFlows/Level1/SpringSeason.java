2991f8ff771a1b59d14515d156123d9aef7e46e4
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-17 14:42:06 +0530

    Update SpringSeason.java

--- src/CoreProgramming/ControlFlows/Level1/SpringSeason.java
+++ src/CoreProgramming/ControlFlows/Level1/SpringSeason.java
@@ -1,3 +1,20 @@
+/*
+ * Problem Statement:
+ * Write a Java program to check whether a given date
+ * falls in the Spring Season. The program should take
+ * month and day as command-line arguments and display
+ * the appropriate result.
+ */
+
+/*
+ * Class Name: SpringSeason
+ *
+ * Description:
+ * This class checks whether a given date falls within
+ * the Spring season based on month and day values
+ * provided through command-line arguments.
+ */
+
 // Class to check Spring Season
 class SpringSeason {
 
