df4225720b9ae9e8b834d06b7e9ca26bc6dc4fb8
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-17 14:27:46 +0530

    Update FactorialFor.java

--- src/CoreProgramming/ControlFlows/Level1/FactorialFor.java
+++ src/CoreProgramming/ControlFlows/Level1/FactorialFor.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+/*
+ * Problem Statement:
+ * Write a Java program to calculate the factorial
+ * of a given positive number using a for loop.
+ */
+
+/*
+ * Class Name: FactorialFor
+ *
+ * Description:
+ * This class reads a number from the user and
+ * calculates its factorial using a for loop,
+ * displaying the result if the number is positive.
+ */
+
 // Class to calculate factorial using for loop
 class FactorialFor {
 
