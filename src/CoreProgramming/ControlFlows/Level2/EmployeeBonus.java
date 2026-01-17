ac885477e156b909b878dc2e5bcaaba35f974d21
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-17 14:46:06 +0530

    Update EmployeeBonus.java

--- src/CoreProgramming/ControlFlows/Level2/EmployeeBonus.java
+++ src/CoreProgramming/ControlFlows/Level2/EmployeeBonus.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+/*
+ * Problem Statement:
+ * Write a Java program to calculate the bonus of an employee
+ * based on years of service. If the employee has more than
+ * 5 years of service, calculate a 5% bonus on salary.
+ */
+
+/*
+ * Class Name: EmployeeBonus
+ *
+ * Description:
+ * This class reads the salary and years of service of an
+ * employee and determines whether the employee is eligible
+ * for a bonus. If eligible, the bonus amount is calculated
+ * and displayed.
+ */
+
 // Class to calculate employee bonus
 class EmployeeBonus {
 
