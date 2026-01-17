bdba5de291f878ad60876149943ef7eb52253649
Wave-Shot <125689235+Wave-Shot@users.noreply.github.com>
2026-01-17 14:39:44 +0530

    Update RocketCountdownWhile.java

--- src/CoreProgramming/ControlFlows/Level1/RocketCountdownWhile.java
+++ src/CoreProgramming/ControlFlows/Level1/RocketCountdownWhile.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+/*
+ * Problem Statement:
+ * Write a Java program to perform a rocket countdown
+ * starting from a given number down to 1 using a while loop.
+ */
+
+/*
+ * Class Name: RocketCountdownWhile
+ *
+ * Description:
+ * This class reads a number from the user and performs
+ * a countdown using a while loop, printing each number
+ * until it reaches 1.
+ */
+
 // Class for rocket countdown
 class RocketCountdownWhile {
 
